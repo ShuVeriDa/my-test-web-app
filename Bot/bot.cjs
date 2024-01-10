12573a90683f3840a4a01e6458aa1141a1eef68d
shuverida <bashtarov@outlook.com>
2024-01-10 15:49:47 +0300

    third commit

--- Bot/bot.cjs
+++ Bot/bot.cjs
@@ -2,7 +2,7 @@ const {Telegraf} = require('telegraf')
 const TOKEN = "6664405728:AAHKI4UmsDZyZ297MPbkvb-FTxzk_8VZD8s"
 const bot = new Telegraf(TOKEN)
 
-const web_link = "http://localhost:5173/"
+const web_link = "https://luxury-jelly-10c540.netlify.app/"
 
 bot.start((ctx) => ctx.reply('Марша дог1ийла!!!', {
   reply_markup: {
